782172babda71856f3d84283b970a3ac38725312
Haldgerd <haldgerd.code@gmail.com>
2022-03-13 20:26:03 +0100

    "Add scss loader to webpack."

--- webpack.config.js
+++ webpack.config.js
@@ -20,20 +20,33 @@ module.exports = {
     path: path.resolve(__dirname, "./dist"),
     clean: true,
   },
-  //   modules suggest types of files and how webpack treats them
+  //   modules suggest types of files to be imported and how webpack treats them
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        include: path.resolve(__dirname, "./src/assets/styles"),
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        include: path.resolve(__dirname, "./src/assets/images"),
         type: "asset/resource",
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        include: path.resolve(__dirname, "./src/assets/fonts"),
+        type: "asset/inline",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
       },
     ],
   },
