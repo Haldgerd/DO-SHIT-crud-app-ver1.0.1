a442579fb016e0a0814799db6e9b01f9ac4358b4
Haldgerd <haldgerd.code@gmail.com>
2022-03-13 14:19:57 +0100

    "Add css loader, scss loader, stsyles loader, assets modules for images, fonts, added dev server. (webpack)."

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
+import "./assets/styles/main.css";
+import newImage from "./assets/images/istockphoto-1338130496-170667a.jpg";
+import Lorna from "./assets/images/tumblr_7335889cb2865853043d9d36a4269c04_12cf37bc_1280.jpg";
+
 const addComponent = () => {
   const element = document.createElement("div");
+  const myImage = new Image();
+  const me = new Image();
 
   element.innerHTML = "This is a test text from index.js file.";
+  element.classList.add("main__title");
+
+  myImage.src = newImage;
+  me.src = Lorna;
+
+  element.appendChild(myImage);
+  element.appendChild(me);
 
   return element;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,40 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
+  // from where to from bundle.js
   entry: "./src/index.js",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "DO SHIT!",
+      template: path.resolve(__dirname, "./public/index.html"), // template file
+      filename: "index.html", // output file
+    }),
+  ],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    clean: true,
+  },
+  //   modules suggest types of files and how webpack treats them
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
