3e67f27b5c302d2dd44caffcf51aba04d606c64d
Haldgerd <haldgerd.code@gmail.com>
2022-03-14 21:27:00 +0100

    "Missing return statement add."

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,10 +3,12 @@ import Lorna from '../assets/images/istockphoto-1338130496-170667a.jpg';
 import '../assets/styles/main.scss';
 
 const Title = () => {
-    <>
-        <h1>This is Webpack</h1>
-        <img src={Lorna} alt="Illustrated bckground." />
-    </>;
+    return (
+        <>
+            <h1>This is Webpack</h1>
+            <img src={Lorna} alt="Illustrated bckground." />
+        </>
+    );
 };
 
 export default Title;
