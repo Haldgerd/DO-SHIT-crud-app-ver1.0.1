a0e1e795d85e162602b67322f725ac44bcad32b2
Haldgerd <haldgerd.code@gmail.com>
2022-03-27 19:52:20 +0200

    "Add react routing for SPA."

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // ------------------- COMPONENTS IMPORT -----------------
 import LandingPage from './views/Landing/LandingPage';
-// import global cross pages styles such as :root ruleset, *, *::before etc.
+import Daily from './views/Daily/Daily';
+import Login from './views/Login/Login';
+import HistoryLog from './views/HistoryLog/HistoryLog';
+import Meetings from './views/Meetings/Meetings';
+import YearGoals from './views/YearGoals/YearGoals';
 import './assets/styles/global.css';
 
 const App = () => {
-    return <LandingPage />;
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<LandingPage />} />
+                <Route exact path="/Login" element={<Login />} />
+                <Route exact path="/DailyShit" element={<Daily />} />
+                <Route exact path="/MeetingsShit" element={<Meetings />} />
+                <Route exact path="/YearShit" element={<YearGoals />} />
+                <Route exact path="/ShitLog" element={<HistoryLog />} />
+                {/* <Route path="*" element={<Error />}/> */}
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
--- src/components/routing/navbar/NavLinks.js
+++ src/components/routing/navbar/NavLinks.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 // --------------- COMPONENTS IMPORT ---------------
 import Button from '../../UI/Button';
@@ -12,38 +12,44 @@ const links = [
         id: 'link1',
         page: 'login',
         context: 'LOGIN ** LOGIN ** LOGIN ** LOGIN ** LOGIN ** LOGIN ** ',
+        path: '/Login',
     },
     {
         id: 'link2',
         page: 'daily',
         context: 'DAILYSHIT ** DAILYSHIT ** DAILYSHIT ** DAILYSHIT ** ',
+        path: '/DailyShit',
     },
     {
         id: 'link3',
         page: 'goals',
         context: 'SHITGOALS ** SHITGOALS ** SHITGOALS ** SHITGOALS ** ',
+        path: '/YearShit',
     },
     {
         id: 'link4',
         page: 'log',
         context: 'SHITLOG ** SHITLOG ** SHITLOG ** SHITLOG ** SHITLOG ** ',
+        path: '/ShitLog',
     },
 ];
 
 const NavLinks = () => {
-    // let navigate = useNavigate();
+    const navigate = useNavigate();
 
     return (
         <ul className={styles.nav__links}>
             {links.map((link) => {
                 return (
-                    <li className={styles.nav__link}>
+                    <li className={styles.nav__link} key={link.id}>
                         <Button
                             buttonClass={styles.nav__button}
                             onClickHandleFunction={() => {
-                                console.log(link.page);
+                                navigate(link.path);
                             }}
-                        />
+                        >
+                            <p />
+                        </Button>
                         <Wrapper wrapperClass={styles.nav__context}>
                             <Paragraph
                                 paragraphClass={styles.nav__text}
